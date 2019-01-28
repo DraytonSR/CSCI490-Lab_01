ed89e695d1e180a4058533266a127418ea8f0056
DraytonScott <42654988+DraytonScott@users.noreply.github.com>
2019-01-28 17:52:19 -0500

    Updated names and started code

---
+++ app/src/main/java/com/example/dscott/simpletimer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dscott.simpletimer2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        }
+    @Override
+    public void onClick(View view){
+
+    }
+}
